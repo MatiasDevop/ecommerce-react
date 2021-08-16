b9892900c7e37bf4350a684bc7b98d49314979ee
Nestor Matias <solomatias321@gmail.com>
2021-08-16 11:59:31 +0200

    adding navbar and better way to import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import Products from './components/Products/Products';
+// import Products from './components/Products/Products';
+// import Navbar from './components/Navbar/Navbar';
  // later on we're gonna change much better way to import components
+import { Products, Navbar } from './components';
 
 const App = () => {
     return (
         <div>
+            <Navbar />
            <Products />
         </div>
     )
