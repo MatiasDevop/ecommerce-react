61957a6af2067dcb69bedb3507a280b816d5777c
Nestor Matias <solomatias321@gmail.com>
2021-08-16 12:11:30 +0200

    fixing image uploaded

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
 
-import logo from '../../assets/commerce.png'
+import logo  from '../../assets/commerce.png';
 import useStyles from './styles';
 
 const Navbar = () => {
@@ -13,7 +13,7 @@ const Navbar = () => {
             <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
                     <Typography variant="h6" className={classes.title} color="inherit">
-                        <img sr={logo} alt="commerce.js" height="25px" className={classes.image} /> Commerce.js
+                        <img src={logo} alt="commerce.js" height="25px" className={classes.image} /> Commerce.js
                     </Typography>
                     <div className={classes.grow} />
                     <div className={classes.button}>
