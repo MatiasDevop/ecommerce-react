260af741027f6d59ec20f6ab9e076e7f2aea2278
Nestor Matias <solomatias321@gmail.com>
2021-08-23 10:49:52 +0200

    working on grid to shipping countries, subdivision and options

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -16,19 +16,46 @@ const AddressForm = ({ checkoutToken }) => {
 
     const methods = useForm();
 
+    const countries = Object.entries(shippingCountries).map(([code, name]) => ({ id: code, label: name}));
+    const subdivisions = Object.entries(shippingSubdivisions).map(([code, name]) => ({ id: code, label: name}));
+    //console.log(countries);
+
     const fetchShippingCountries = async (checkoutTokenId) => {
         console.log(checkoutTokenId);
       
         const { countries }  = await commerce.services.localeListShippingCountries(checkoutTokenId);
-        console.log(countries);
-        
         //console.log(countries);
+        
         setShippingCountries(countries);
+        setShippingCountry(Object.keys(countries)[0]);
+    }
+
+    const fetchSubdivisions = async (countryCode) => {
+        const { subdivisions } = await commerce.services.localeListSubdivisions(countryCode);
+
+        setShippingSubdivisions(subdivisions);
+        setShippingSubdivision(Object.keys(subdivisions)[0]);
+    }
+
+    const fetchShippingOptions = async (checkoutTokenId, country, region = null) => {
+        const options = await commerce.checkout.getShippingOptions(checkoutTokenId, { country, region });
+
+        setShippingOptions(options);
+        setShippingOption(options[0].id);
     }
     
+    // you can have more than one useEffects
     useEffect(() => {
         fetchShippingCountries(checkoutToken.id);
     }, []);
+
+    useEffect(() => {
+        if(shippingCountry) fetchSubdivisions(shippingCountry);
+    }, [shippingCountry]);
+
+    useEffect(() => {
+        if(shippingSubdivision) fetchShippingOptions(checkoutToken.id);
+    }, [shippingSubdivision])
 
     return (
         <>
@@ -42,23 +69,27 @@ const AddressForm = ({ checkoutToken }) => {
                         <FormInput required name='email' label='Email'/>
                         <FormInput required name='city' label='City'/>
                         <FormInput required name='zip' label='Zip / Postal Code'/>
-                        {/* <Grid item xs={12} sm={6}>
+                        <Grid item xs={12} sm={6}>
                             <InputLabel>Shipping Country</InputLabel>
-                            <Select value={} fullWidth onChange={}>
-                                <MenuItem key={} value={}>
-                                    select me
-                                </MenuItem>
+                            <Select value={shippingCountry} fullWidth onChange={(e) => setShippingCountry(e.target.value)}>
+                                {countries.map((country) => (
+                                    <MenuItem key={country.id} value={country.id}>
+                                        {country.label}
+                                    </MenuItem> 
+                                ))}
                             </Select>
                         </Grid>
                         <Grid item xs={12} sm={6}>
                             <InputLabel>Shipping SubDivision</InputLabel>
-                            <Select value={} fullWidth onChange={}>
-                                <MenuItem key={} value={}>
-                                    select me
-                                </MenuItem>
+                            <Select value={shippingSubdivision} fullWidth onChange={(e) => setShippingSubdivision(e.target.value) }>
+                                {subdivisions.map((subdivision) => (
+                                    <MenuItem key={subdivision.id} value={subdivision.id}>
+                                        {subdivision.label}
+                                    </MenuItem> 
+                                ))} 
                             </Select>
                         </Grid>
-                        <Grid item xs={12} sm={6}>
+                        {/* <Grid item xs={12} sm={6}>
                             <InputLabel>Shipping Options</InputLabel>
                             <Select value={} fullWidth onChange={}>
                                 <MenuItem key={} value={}>
